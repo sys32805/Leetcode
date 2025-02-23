15847d3ace68b8fa8b7697a9c0c0b108c8e36870
Rohit Yadav <65734739+sys32805@users.noreply.github.com>
2025-02-23 19:10:10 +0530

    Time: 1 ms (100%), Space: 46 MB (83.55%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,33 +1,34 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        int len = n1 + n2;
-        int[] arr3 = new int[len];
-        int i = 0, j = 0, k = 0;
+    public double findMedianSortedArrays(int[] input1, int[] input2) {
+        if (input1.length > input2.length) {
+            return findMedianSortedArrays(input2, input1);
+        }
+        int x = input1.length;
+        int y = input2.length;
 
-        while (i < n1 && j < n2) {
-            if (nums1[i] < nums2[j]) {
-                arr3[k++] = nums1[i++];
-            } else {
-                arr3[k++] = nums2[j++];
+        int low = 0;
+        int high = x;
+        while (low <= high) {
+            int partitionX = (low + high)/2;
+            int partitionY = (x + y + 1)/2 - partitionX;
+            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : input1[partitionX - 1];
+            int minRightX = (partitionX == x) ? Integer.MAX_VALUE : input1[partitionX];
+
+            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : input2[partitionY - 1];
+            int minRightY = (partitionY == y) ? Integer.MAX_VALUE : input2[partitionY];
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                if ((x + y) % 2 == 0) {
+                    return ((double)Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY))/2;
+                } else {
+                    return (double)Math.max(maxLeftX, maxLeftY);
+                }
+            } else if (maxLeftX > minRightY) {
+                high = partitionX - 1;
+            } else { 
+                low = partitionX + 1;
             }
         }
-
-        while (i < n1) {
-            arr3[k++] = nums1[i++];
-        }
-
-        while (j < n2) {
-            arr3[k++] = nums2[j++];
-        }
-
-        if (len % 2 == 1) {
-            return arr3[len / 2];
-        } else {
-            int mid1 = len / 2;
-            int mid2 = mid1 - 1;
-            return (arr3[mid1] + arr3[mid2]) / 2.0;
-        }
+        return -1;
     }
 }
