cae6b73cf7fbd536bcdea4b6e5472d7398a40867
Rohit Yadav <65734739+sys32805@users.noreply.github.com>
2025-02-25 23:25:51 +0530

    Time: 4 ms (1.37%), Space: 45 MB (25.19%) - LeetHub

---
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int  j = 0;
+        int index = 0;
+        int a[] = new int[nums.length];
+        while(j!= n){
+           a[index++] = nums[j];
+           a[index++] = nums[j+n]; 
+            j++;
+        }
+        System.out.println(Arrays.toString(a));
+        return a;
+    }
+}
